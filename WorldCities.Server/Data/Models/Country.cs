0d6297517d1fbe6035dd43072505c6f28852517d
qiuxan <qiuxan@gmail.com>
2024-05-21 21:05:24 +1000

    setup model

---
+++ WorldCities.Server/Data/Models/Country.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace WorldCities.Server.Data.Models
+{
+    public class Country
+    {
+        #region Properties
+        /// <summary>
+        /// The unique id and primary key for this Country
+        /// </summary>
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        /// <summary>
+        /// Country name (in UTF8 format)
+        /// </summary>
+        public required string Name { get; set; }
+        /// <summary>
+        /// Country code (in ISO 3166-1 ALPHA-2 format)
+        /// </summary>
+        public required string ISO2 { get; set; }
+        /// <summary>
+        /// Country code (in ISO 3166-1 ALPHA-3 format)
+        /// </summary>
+        public required string ISO3 { get; set; }
+        #endregion
+
+        #region Navigation Properties
+        /// <summary>
+        /// A collection of all the cities related to this country.
+        /// </summary>
+        public ICollection<City>? Cities { get; set; }
+        #endregion
+    }
+}
