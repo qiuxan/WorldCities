a4f52b589fe51b44c25b2663af982773cf7b4609
qiuxan <qiuxan@gmail.com>
2024-05-23 20:04:10 +1000

    set up pagination in server

--- WorldCities.Server/Controllers/CitiesController.cs
+++ WorldCities.Server/Controllers/CitiesController.cs
@@ -23,9 +23,13 @@ namespace WorldCities.Server.Controllers
 
         // GET: api/Cities
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<City>>> GetCities()
+        public async Task<ActionResult<ApiResult<City>>> GetCities(int pageIndex = 0,int pageSize = 10)
         {
-            return await _context.Cities.ToListAsync();
+            return await ApiResult<City>.CreateAsync(
+                _context.Cities.AsNoTracking(),
+                pageIndex,
+                pageSize
+                );
         }
 
         // GET: api/Cities/5
