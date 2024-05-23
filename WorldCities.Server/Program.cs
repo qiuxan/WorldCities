9a2dd31a8f8e0baf9251c87175984d5cfa020415
qiuxan <qiuxan@gmail.com>
2024-05-23 15:06:33 +1000

    Add controller and format it so it is more readable.

--- WorldCities.Server/Program.cs
+++ WorldCities.Server/Program.cs
@@ -12,7 +12,10 @@ namespace WorldCities.Server
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.WriteIndented = true;
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
