e11b58c74ef7111be51fb1cffa089606ce8aae92
qiuxan <qiuxan@gmail.com>
2024-05-23 15:50:31 +1000

    init AngularMaterialModule

--- worldcities.client/src/app/app.module.ts
+++ worldcities.client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularMaterialModule } from './angular-material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +22,9 @@ import { CitiesComponent } from './cities/cities.component';
     CitiesComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,
+    BrowserModule,
+    AngularMaterialModule,
+    HttpClientModule,
     AppRoutingModule,
     MatButtonModule,
     MatIconModule,
