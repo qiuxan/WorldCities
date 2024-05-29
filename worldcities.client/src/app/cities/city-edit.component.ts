cb0d49418268287bb1b33dd44414a45f18f4e73c
qiuxan <qiuxan@gmail.com>
2024-05-29 22:30:36 +1000

    server-side validation

--- worldcities.client/src/app/cities/city-edit.component.ts
+++ worldcities.client/src/app/cities/city-edit.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl, AsyncValidatorFn } from '@angular/forms';
 import { environment } from './../../environments/environment';
 import { City } from './city';
 import { Country } from './../countries/country';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-city-edit',
@@ -31,16 +33,35 @@ export class CityEditComponent implements OnInit {
     private router: Router,
     private http: HttpClient) {
   }
+
+  
   ngOnInit() {
     this.form = new FormGroup({
       name: new FormControl('', Validators.required),
       lat: new FormControl('', Validators.required),
       lon: new FormControl('', Validators.required),
       countryId: new FormControl('', Validators.required)
-    });
+    }, null, this.isDupeCity());
 
     this.loadData();
   }
+
+  isDupeCity(): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<{ [key: string]: any } |
+      null> => {
+      var city = <City>{};
+      city.id = (this.id) ? this.id : 0;
+      city.name = this.form.controls['name'].value;
+      city.lat = +this.form.controls['lat'].value;
+      city.lon = +this.form.controls['lon'].value;
+      city.countryId = +this.form.controls['countryId'].value;
+      var url = environment.baseUrl + 'api/Cities/IsDupeCity';
+      return this.http.post<boolean>(url, city).pipe(map(result => {
+        return (result ? { isDupeCity: true } : null);
+      }));
+    }
+  }
+
   loadData() {
 
     // load countries
