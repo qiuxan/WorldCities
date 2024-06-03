5be86d11c674f425753c3099adb72c5d317a187b
qiuxan <qiuxan@gmail.com>
2024-06-03 22:35:00 +1000

    Logging HTTP requests to SQL Server

--- WorldCities.Server/Program.cs
+++ WorldCities.Server/Program.cs
@@ -2,6 +2,10 @@
 using Microsoft.EntityFrameworkCore;
 using WorldCities.Server.Data;
 
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.MSSqlServer;
+
 namespace WorldCities.Server
 {
     public class Program
@@ -9,6 +13,21 @@ namespace WorldCities.Server
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Adds Serilog support
+            builder.Host.UseSerilog((ctx, lc) => lc
+            .ReadFrom.Configuration(ctx.Configuration)
+            .WriteTo.MSSqlServer(connectionString:
+            ctx.Configuration.GetConnectionString("DefaultConnection"),
+            restrictedToMinimumLevel: LogEventLevel.Information,
+            sinkOptions: new MSSqlServerSinkOptions
+            {
+                TableName = "LogEvents",
+                AutoCreateSqlTable = true
+            }
+            )
+            .WriteTo.Console()
+            );
 
             // Add services to the container.
 
@@ -28,6 +47,8 @@ namespace WorldCities.Server
             );
 
             var app = builder.Build();
+
+            app.UseSerilogRequestLogging();
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
