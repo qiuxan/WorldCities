7fce5044304741904a7ca6453661231074500cb7
qiuxan <qiuxan@gmail.com>
2024-06-04 12:11:05 +1000

    First unit test to test the GetCity action of the citiescontroller

---
+++ WorldCities.Server.Tests/CitiesController_Test.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using WorldCities.Server.Controllers;
+using WorldCities.Server.Data;
+using WorldCities.Server.Data.Models;
+using Xunit;
+namespace WorldCities.Server.Tests
+{
+    public class CitiesController_Tests
+    {
+        /// <summary>
+        /// Test the GetCity() method
+        /// </summary>
+        [Fact]
+        public async Task GetCity()
+        {
+            // Arrange
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "WorldCities")
+            .Options;
+            using var context = new ApplicationDbContext(options);
+            context.Add(new City()
+            {
+                Id = 1,
+                CountryId = 1,
+                Lat = 1,
+                Lon = 1,
+                Name = "TestCity1"
+            });
+            context.SaveChanges();
+            var controller = new CitiesController(context);
+            City? city_existing = null;
+            City? city_notExisting = null;
+
+            // todo: define the required assets
+            // Act
+            city_existing = (await controller.GetCity(1)).Value;
+            city_notExisting = (await controller.GetCity(2)).Value;
+            // todo: invoke the test
+            // Assert
+            Assert.NotNull(city_existing);
+            Assert.Null(city_notExisting);
+
+            // todo: verify that conditions are met.
+        }
+    }
+}
